fc1395b297ed60a33f4ad20e72ebd68faf28ed06
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-22 03:59:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ def find_the_cheese(array)# code an argument here
       array.include?("cheddar" || "gouda" || "camembert")
       array.detect do |cheese|
       cheese == "cheddar" || "gouda" || "camembert"
-      end
+    
       cheese
     end
 end
