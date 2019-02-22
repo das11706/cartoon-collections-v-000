bac05fb647173f99610c815becd116f6eb748fc7
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-22 03:33:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,7 +26,8 @@ def find_the_cheese(array)# code an argument here
       array.include?("cheddar" || "gouda" || "camembert")
       #array.detect{|cheese| cheese.is_a?("cheddar")}
       #cheese.include?("cheddar" || "gouda" || "camembert")
-      array.detect{|cheese| cheese}
+      array.detect{|cheese| cheese == array}
+    end
     
 end
 #[1,2,3,4].detect{|i| i.even?} #=> 2
