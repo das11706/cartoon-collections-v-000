3962c32a5bcbcea06c06f658d13543f4fe453b53
Dan Suarez <suarez.daniel101@gmail.com>
2019-02-22 03:35:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -26,8 +26,9 @@ def find_the_cheese(array)# code an argument here
       array.include?("cheddar" || "gouda" || "camembert")
       #array.detect{|cheese| cheese.is_a?("cheddar")}
       #cheese.include?("cheddar" || "gouda" || "camembert")
-      array.detect{|cheese| cheese == array}
-    
+      array.detect do |cheese| 
+       cheese == array.include?("cheddar" || "gouda" || "camembert")
+    end
 end
 #[1,2,3,4].detect{|i| i.even?} #=> 2
 #[1,2,3,4].detect{|i| i.is_a?(String)} #=> nil
